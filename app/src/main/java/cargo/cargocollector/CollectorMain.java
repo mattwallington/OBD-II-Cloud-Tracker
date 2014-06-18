2789288bd5bae85f93fdbfa733115989dd3ae1c7
Matt Wallington <matt@cpusage.com>
2014-06-18 06:51:51 -0700

    Updated to add Bluetooth and socket communication

--- app/src/main/java/cargo/cargocollector/CollectorMain.java
+++ app/src/main/java/cargo/cargocollector/CollectorMain.java
@@ -2,7 +2,8 @@ package cargo.cargocollector;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -21,11 +22,13 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.support.v4.content.LocalBroadcastManager;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.UUID;
 
 import cargo.cargocollector.R;
 
-public class StartActivity extends Activity implements LocationListener, SensorEventListener {
+public class CollectorMain extends Activity implements LocationListener, SensorEventListener {
 
     private TextView tv;
 
@@ -42,17 +45,25 @@ public class StartActivity extends Activity implements LocationListener, SensorE
 
     /* Bluetooth / OBD2 */
     private BluetoothAdapter btadapter;
+    private BluetoothSocket btsocket;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        tv = (TextView) findViewById(R.id.service_status);
+
         //Activate location tracking.
         activateLocation();
 
         //Activate Accelerometer
         activateAccelerometer();
+
+        //Activate OBD
+        getOBD();
+
     }
 
     @Override
@@ -107,13 +118,22 @@ public class StartActivity extends Activity implements LocationListener, SensorE
         }
 
         locationManager.requestLocationUpdates(provider, MIN_TIME, MIN_DISTANCE, this);
+
     }
 
     @Override
     public void onLocationChanged(Location location) {
         //deal with location.
-        String loc = Double.toString(location.getLatitude()) + ", " + Double.toString(location.getLongitude());
-        tv.append(loc + "\n");
+        try {
+            String point = Double.toString(location.getLatitude()) + ", " + Double.toString(location.getLongitude());
+            tv.append(point + "\n");
+            Log.d("Location", point);
+            //TODO: Do something with location data.
+
+        }
+        catch (Exception e) {
+            Log.d("Exception", e.getMessage());
+        }
     }
 
     @Override
@@ -144,8 +164,9 @@ public class StartActivity extends Activity implements LocationListener, SensorE
     private void activateAccelerometer() {
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Log.d("Status", "Going in.");
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
+        Log.d("Status", "Returned");
 
     }
 
@@ -168,6 +189,7 @@ public class StartActivity extends Activity implements LocationListener, SensorE
      *  Bluetooth / OBD2
      */
     private void getOBD() {
+        Log.d("Method", "getOBD()");
         //Get bluetooth adapter
         btadapter = BluetoothAdapter.getDefaultAdapter();
         if (btadapter == null) {
@@ -179,7 +201,32 @@ public class StartActivity extends Activity implements LocationListener, SensorE
             return;
         }
 
-        final UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+        //final UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
+        /*
+        Set<BluetoothDevice> pairedDevices;
+        pairedDevices = btadapter.getBondedDevices();
+
+        for (Iterator<BluetoothDevice> iter = pairedDevices.iterator(); iter.hasNext();) {
+            BluetoothDevice device = iter.next();
+            Log.d("BluetoothDevice", device.getName() + ": " + device.getAddress() + "; " +);
+
+        }
+        */
+
+        //BT Device: "OBDLink MX" 00:04:3E:30:94:66
+        BluetoothDevice device = btadapter.getRemoteDevice("00:04:3E:30:94:66");
+
+        try {
+            btsocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            btsocket.connect();
+        }
+        catch (Exception e) {
+            Log.d("Exception", e.getMessage());
+        }
+
+        // Read this site:  http://www.codota.com/android/scenarios/51891a46da0a6c5c8ff8531f/android.bluetooth.BluetoothSocket?tag=out_2013_05_05_07_19_34
+
+        // You need to implement an inputstream and outputstream.
     }
 }
