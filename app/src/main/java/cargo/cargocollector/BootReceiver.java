fd47469a8c4a2241f330b958fa822425305bb17f
Matt Wallington <mattwallington@gmail.com>
2015-03-03 10:09:02 -0800

    Refactored bootreceiver specifically for CollectorService

--- app/src/main/java/cargo/cargocollector/BootReceiver.java
+++ app/src/main/java/cargo/cargocollector/BootReceiver.java
@@ -8,15 +8,15 @@ import android.content.Intent;
  * Created by matt on 7/16/14.
  */
 public class BootReceiver extends BroadcastReceiver {
-    ServiceInitiator m_initiator;
+    CollectorServiceInitiator m_collectorServiceInitiator;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        m_initiator = new ServiceInitiator(context);
+        m_collectorServiceInitiator = new CollectorServiceInitiator(context);
 
-        m_initiator.initiateService();
+        m_collectorServiceInitiator.initiateService(CollectorService.class);
 
-        m_initiator.start();
+        m_collectorServiceInitiator.start();
     }
 }
