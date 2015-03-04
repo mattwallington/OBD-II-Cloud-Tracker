bd6859a8a70234719310a10b7b2c0b862c1513a1
Matt Wallington <mattwallington@gmail.com>
2015-03-04 10:15:30 -0800

    Cleaned up services

--- app/src/main/java/cargo/cargocollector/BootReceiver.java
+++ app/src/main/java/cargo/cargocollector/BootReceiver.java
@@ -1,22 +1,35 @@
 package cargo.cargocollector;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by matt on 7/16/14.
  */
 public class BootReceiver extends BroadcastReceiver {
-    CollectorServiceInitiator m_collectorServiceInitiator;
+    public CollectorService m_boundService = null;
+    public ServiceConnection m_connection = null;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        m_collectorServiceInitiator = new CollectorServiceInitiator(context);
+        context.startService(new Intent(context, CollectorService.class));
+        m_connection = new ServiceConnection() {
+            public void onServiceConnected(ComponentName className, IBinder service) {
+                m_boundService = ((CollectorService.CollectorBinder) service).getService();
 
-        m_collectorServiceInitiator.initiateService(CollectorService.class);
+                Log.d("CollectorService", "Service connected." + className.toString());
+            }
 
-        m_collectorServiceInitiator.start();
+            public void onServiceDisconnected(ComponentName className) {
+                Log.d("CollectorService", "Service disconnected");
+                m_boundService = null;
+            }
+        };
     }
 }
