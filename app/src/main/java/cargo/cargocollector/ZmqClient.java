3e994e1b3502d948c29b159e097408f01f0fe268
Matt Wallington <mattwallington@gmail.com>
2015-03-08 18:09:38 -0700

    Completed functionality.  It properly sends messages to the server now

--- app/src/main/java/cargo/cargocollector/ZmqClient.java
+++ app/src/main/java/cargo/cargocollector/ZmqClient.java
@@ -5,55 +5,96 @@ import android.util.Log;
 
 import org.jeromq.ZMQ;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by mattwallington on 3/1/15.
  */
 public class ZmqClient {
-    private static ZMQ.Context m_context;
-    private static ZMQ.Socket m_requester;
-    private static String m_host;
-    private static String m_port;
+    private static String TAG;
+    private static final String HOST = "zmq.spartacus.io";
+    private static final String PORT = "5555";
+    private ZMQ.Context mContext;
+    private LinkedBlockingQueue<byte[]> mQueue;
+    private boolean mQuitThread;
+    //private ProcessSendQueue mProcessSendQueue;
 
-    public static void connect(final String data) {
 
+    public ZmqClient() {
+        TAG = this.getClass().getSimpleName();
+        mContext = ZMQ.context(1);
+        mQueue = new LinkedBlockingQueue<byte[]>();
+        mQuitThread = false;
+
+        //connect();
+    }
+
+    public void connect() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                m_context = ZMQ.context(1);
-
-                m_requester = m_context.socket(ZMQ.REQ);
-
-                m_requester.connect("tcp://" + m_host + ":" + m_port);
-
-                m_requester.send(data.getBytes(), 0);
-
-                byte[] reply = m_requester.recv(0);
-                Log.d("ZMQ", "Data: " + reply.toString());
+                ZMQ.Socket requester = mContext.socket(ZMQ.REQ);
+                requester.connect("tcp://" + HOST + ":" + PORT);
             }
         }).start();
     }
 
-    public static void send(final String data) {
-
+    public void start() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                m_requester.send(data.getBytes(), 0);
+                Log.d(TAG, "Start ZmqClient thread.");
+                ZMQ.Socket requester = mContext.socket(ZMQ.REQ);
+                requester.connect("tcp://" + HOST + ":" + PORT);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e){
+                    Log.e(TAG, "Thread interruption exception", e);
+                }
 
-                byte[] reply = m_requester.recv(0);
-                Log.d("ZMQ", "Data: " + reply.toString());
+                byte[] data = null;
+                while(!mQuitThread) {
+                    Log.d(TAG, "--->Thread fired.");
+                    try {
+                        data = mQueue.poll(5000, TimeUnit.MILLISECONDS);
+                    } catch (InterruptedException e) {
+                        Log.e(TAG, "Queue take interrupted.", e);
+                        break;
+                    }
+                    try {
+                        boolean resp = requester.send(data, 0);
+
+                        Log.d(TAG, "Sent.  Resp: "+resp);
+                        //String throwAway = requester.recvMsg(0);
+                        ZMQ.Msg throwAway = requester.recvMsg(0);
+                        //byte[] throwAway = requester.recv(0);
+
+                        Log.d(TAG, "Received: "+throwAway.toString());
+                    } catch (Exception e) {
+                        Log.e(TAG, "Exception sending data.", e);
+                    }
+
+                }
+                Log.d(TAG, "Closing sockets.");
+                requester.close();
+                mContext.term();
+                Log.d(TAG, "END ZmqClient thread.");
             }
         }).start();
     }
 
-    public static byte[] receive() {
-        byte[] reply = m_requester.recv(0);
-        return reply;
+    public void cancel() {
+        mQuitThread = true;
     }
 
-    public static void setCredentials(String host, String port) {
-        m_host = host;
-        m_port = port;
+    public void send(byte[] msg) {
+        try {
+            //Log.d(TAG, "Adding msg to queue: " + msg);
+            mQueue.offer(msg, 10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            Log.e(TAG, "Queue put interrupted.", e);
+        }
     }
-
 }
