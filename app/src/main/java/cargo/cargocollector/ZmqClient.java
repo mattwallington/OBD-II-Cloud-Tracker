98df27a5233236022b8e8dd650cf6a97958289e4
Matt Wallington <mattwallington@gmail.com>
2015-03-09 01:19:17 -0700

    Added receive timeout. Track queue growth.  Recover from random server errors

--- app/src/main/java/cargo/cargocollector/ZmqClient.java
+++ app/src/main/java/cargo/cargocollector/ZmqClient.java
@@ -1,11 +1,12 @@
 package cargo.cargocollector;
 
 
+import android.content.Context;
 import android.util.Log;
 
 import org.jeromq.ZMQ;
+import org.jeromq.ZMQException;
 
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
@@ -14,73 +15,97 @@ import java.util.concurrent.TimeUnit;
  */
 public class ZmqClient {
     private static String TAG;
-    private static final String HOST = "zmq.spartacus.io";
-    private static final String PORT = "5555";
-    private ZMQ.Context mContext;
+    private String mServerUri;
+    private static final int RCV_TIMEOUT = 10000;
+    private ZMQ.Context mZmqContext;
     private LinkedBlockingQueue<byte[]> mQueue;
     private boolean mQuitThread;
     //private ProcessSendQueue mProcessSendQueue;
 
 
-    public ZmqClient() {
+    public ZmqClient(Context context) {
+        String host = null;
+        String port = null;
+
         TAG = this.getClass().getSimpleName();
-        mContext = ZMQ.context(1);
+        mZmqContext = ZMQ.context(1);
         mQueue = new LinkedBlockingQueue<byte[]>();
         mQuitThread = false;
 
-        //connect();
+        host = context.getString(R.string.host);
+        port = context.getString(R.string.port);
+
+        mServerUri = "tcp://" + host + ":" + port;
+        Log.d(TAG, "Server URI: "+mServerUri);
     }
 
-    public void connect() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                ZMQ.Socket requester = mContext.socket(ZMQ.REQ);
-                requester.connect("tcp://" + HOST + ":" + PORT);
-            }
-        }).start();
-    }
 
     public void start() {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 Log.d(TAG, "Start ZmqClient thread.");
-                ZMQ.Socket requester = mContext.socket(ZMQ.REQ);
-                requester.connect("tcp://" + HOST + ":" + PORT);
                 try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e){
-                    Log.e(TAG, "Thread interruption exception", e);
-                }
+                    ZMQ.Socket requester = mZmqContext.socket(ZMQ.REQ);
 
-                byte[] data = null;
-                while(!mQuitThread) {
-                    Log.d(TAG, "--->Thread fired.");
+                    //Set recv timeout
+                    requester.setReceiveTimeOut(RCV_TIMEOUT);
+
+                    requester.connect(mServerUri);
                     try {
-                        data = mQueue.poll(5000, TimeUnit.MILLISECONDS);
-                    } catch (InterruptedException e) {
-                        Log.e(TAG, "Queue take interrupted.", e);
-                        break;
-                    }
-                    try {
-                        boolean resp = requester.send(data, 0);
-
-                        Log.d(TAG, "Sent.  Resp: "+resp);
-                        //String throwAway = requester.recvMsg(0);
-                        ZMQ.Msg throwAway = requester.recvMsg(0);
-                        //byte[] throwAway = requester.recv(0);
-
-                        Log.d(TAG, "Received: "+throwAway.toString());
-                    } catch (Exception e) {
-                        Log.e(TAG, "Exception sending data.", e);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                        Log.e(TAG, "Thread interruption exception", e);
                     }
 
+                    byte[] data = null;
+                    while(!mQuitThread) {
+                        Log.d(TAG, "--->Thread fired.");
+                        try {
+                            data = mQueue.poll(5000, TimeUnit.MILLISECONDS);
+                        } catch (InterruptedException e) {
+                            Log.e(TAG, "Queue take interrupted.", e);
+                            break;
+                        }
+                        try {
+                            boolean resp = requester.send(data, 0);
+
+                            Log.d(TAG, "Sent.  Resp: "+resp);
+
+                            ZMQ.Msg throwAway = null;
+                            try {
+                                throwAway = requester.recvMsg(0);
+                                Log.d(TAG, "Received: "+throwAway.toString());
+                            } catch (ZMQException e) {
+                                Log.e(TAG, "ZMQ receive hit timeout.  Restarting socket.");
+                                requester.close();
+                                requester.connect(mServerUri);
+                            }
+
+                        } catch (Exception e) {
+                            Log.e(TAG, "Exception sending data.", e);
+                            requester.close();
+                            mZmqContext.term();
+                            mZmqContext = ZMQ.context(1);
+                            requester = mZmqContext.socket(ZMQ.REQ);
+                            requester.setReceiveTimeOut(RCV_TIMEOUT);
+                            requester.connect(mServerUri);
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException ie){
+                                Log.e(TAG, "Thread interruption exception", ie);
+                            }
+                        }
+                        Log.v(TAG, "ZmqQueue Length: "+mQueue.size());
+                    }
+                    Log.d(TAG, "Closing sockets.");
+                    requester.close();
+                    mZmqContext.term();
+                    Log.d(TAG, "END ZmqClient thread.");
+                } catch (ZMQException e){
+                    Log.e(TAG, "ZMQ Exception", e);
                 }
-                Log.d(TAG, "Closing sockets.");
-                requester.close();
-                mContext.term();
-                Log.d(TAG, "END ZmqClient thread.");
+
             }
         }).start();
     }
