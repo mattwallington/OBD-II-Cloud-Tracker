5a0ab1de30e5c79b05056f4b864aac689b559db3
Matt Wallington <mattwallington@gmail.com>
2015-04-01 22:46:11 -0700

    Updated variable names for readability and consistency

--- app/src/main/java/cargo/cargocollector/BootReceiver.java
+++ app/src/main/java/cargo/cargocollector/BootReceiver.java
@@ -12,23 +12,23 @@ import android.util.Log;
  * Created by matt on 7/16/14.
  */
 public class BootReceiver extends BroadcastReceiver {
-    public CollectorService m_boundService = null;
-    public ServiceConnection m_connection = null;
+    public CollectorService mBoundService = null;
+    public ServiceConnection mConnection = null;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         context.startService(new Intent(context, CollectorService.class));
-        m_connection = new ServiceConnection() {
+        mConnection = new ServiceConnection() {
             public void onServiceConnected(ComponentName className, IBinder service) {
-                m_boundService = ((CollectorService.CollectorBinder) service).getService();
+                mBoundService = ((CollectorService.CollectorBinder) service).getService();
 
                 Log.d("CollectorService", "Service connected." + className.toString());
             }
 
             public void onServiceDisconnected(ComponentName className) {
                 Log.d("CollectorService", "Service disconnected");
-                m_boundService = null;
+                mBoundService = null;
             }
         };
     }
